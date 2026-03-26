08897b7d9efef00087d4dd0352308606e74e176b
cleitinho1234 <cleitinhodacruzsilva4@gmail.com>
2026-03-26 20:36:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// 🔥 CONEXÃO COM MONGODB
 mongoose.connect("mongodb+srv://admin:123456mini@cluster0.j6xbddq.mongodb.net/miniZap?retryWrites=true&w=majority")
 .then(() => console.log("Mongo conectado"))
 .catch(err => console.log(err));
 
-// MODELOS
 const User = mongoose.model("User", {
     id: String,
     username: String,
@@ -26,7 +24,6 @@ const Message = mongoose.model("Message", {
     timestamp: Number
 });
 
-// SALVAR PERFIL
 app.post("/saveProfile", async (req, res) => {
     const { id, username, photo } = req.body;
 
@@ -39,7 +36,6 @@ app.post("/saveProfile", async (req, res) => {
     res.send({ success: true });
 });
 
-// PEGAR USUÁRIO
 app.get("/getUser/:id", async (req, res) => {
     const user = await User.findOne({ id: req.params.id });
 
@@ -47,7 +43,6 @@ app.get("/getUser/:id", async (req, res) => {
     else res.status(404).send({ error: "Usuário não encontrado" });
 });
 
-// ENVIAR MENSAGEM
 app.post("/sendMessage", async (req, res) => {
     const { fromId, toId, text } = req.body;
 
@@ -61,7 +56,6 @@ app.post("/sendMessage", async (req, res) => {
     res.send({ success: true });
 });
 
-// RECEBER MENSAGENS
 app.get("/getMessages/:id", async (req, res) => {
     const id = req.params.id;
 
